d39db8e0da5e5a1e14033c03cdcbd1f1a1406c3e
Deepak Naik <deepak@digite.com>
2023-05-05 10:12:19 +0530

    Updated terraform file and Deploy instructions

--- README.md
+++ README.md
@@ -12,3 +12,21 @@ brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 ```
 
+### Install
+
+Create infra
+
+Verify the script
+```sh
+terraform init
+```
+
+Verify the deployment / Changes plan
+```sh
+terraform plan -out=tfplan
+```
+
+Apply the changes
+```sh
+terraform apply "tfplan"
+```
--- infra/main.tf
+++ infra/main.tf
@@ -48,25 +48,25 @@ resource "aws_api_gateway_rest_api" "lms_api" {
 }
 
 # Create API Gateway resource
-resource "aws_api_gateway_resource" "my_resource" {
+resource "aws_api_gateway_resource" "lms_resource" {
   rest_api_id = aws_api_gateway_rest_api.lms_api.id
   parent_id   = aws_api_gateway_rest_api.lms_api.root_resource_id
   path_part   = "lmsresource"
 }
 
 # Create API Gateway method
-resource "aws_api_gateway_method" "my_method" {
+resource "aws_api_gateway_method" "lms_method" {
   rest_api_id   = aws_api_gateway_rest_api.lms_api.id
-  resource_id   = aws_api_gateway_resource.my_resource.id
+  resource_id   = aws_api_gateway_resource.lms_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
 # Create API Gateway integration
-resource "aws_api_gateway_integration" "my_integration" {
+resource "aws_api_gateway_integration" "lms_integration" {
   rest_api_id = aws_api_gateway_rest_api.lms_api.id
-  resource_id = aws_api_gateway_resource.my_resource.id
-  http_method = aws_api_gateway_method.my_method.http_method
+  resource_id = aws_api_gateway_resource.lms_resource.id
+  http_method = aws_api_gateway_method.lms_method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -84,4 +84,21 @@ resource "aws_lambda_permission" "api_gateway" {
 }
 
 # Deploy API Gateway
-resource "aws_api_gateway_deployment"
+resource "aws_api_gateway_deployment" "lms_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.lms_api.id
+  stage_name  = "prod"
+
+  # Ensure the deployment updates when the API configuration changes
+  triggers = {
+    redeployment = sha1(
+      jsonencode(
+        aws_api_gateway_integration.lms_integration
+      )
+    )
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.lms_integration,
+  ]
+}
+
